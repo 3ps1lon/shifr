4c2b6da39097d7ba22f7b782566cabda83e2a887
3ps1lon <37895445+3ps1lon@users.noreply.github.com>
2022-01-09 22:13:57 +0700

    Update shifr.cpp

--- shifr.cpp
+++ shifr.cpp
@@ -1,9 +1,9 @@
-﻿#include "Header.h"
+#include "Header.h"
 
 
 int main() {
 
-	enc("d d.da/sda","test",45,3);       // word, file_name, seed_random, leftshift
+	enc("d d.da/sda","test",45,4);       // word, file_name, seed_random, leftshift
 	decr("test",45,3);					// file_name, seed_random, leftshift
 	
 	return 0;
