ccd2b51d1cbc095b01371e7193d0979c83e3330c
3ps1lon <37895445+3ps1lon@users.noreply.github.com>
2022-01-09 22:13:36 +0700

    Update encrypt.cpp

--- encrypt.cpp
+++ encrypt.cpp
@@ -15,23 +15,31 @@ void enc(std::string word, std::string file_name, unsigned int key, unsigned int
 	std::ofstream file;
 	file.open(file_name, std::ios::binary);
 
-	for (int i = 0; i < byte.size(); i += 2) {
+	for (int i = 0; i < byte.size(); i += 4) {
 		unsigned int gamma = rand();
-		unsigned char byte1 = byte[i];
-		unsigned char byte2 = i + 1 < byte.size() ? byte[i + 1] : 0u;
+		unsigned char b0 = byte[i];
+		unsigned char b1 = i + 1 < byte.size() ? byte[i + 1] : 0u;
+		unsigned char b2 = i + 2 < byte.size() ? byte[i + 2] : 0u;
+		unsigned char b3 = i + 3 < byte.size() ? byte[i + 3] : 0u;
+		unsigned int message_to_int =
+			((static_cast<unsigned int>(b0) << 24u) | (static_cast<unsigned int>(b1) << 16u) |
+				(static_cast<unsigned int>(b2) << 8u) | (static_cast<unsigned int>(b3)));
 
-		unsigned int message_xored = ((static_cast<unsigned int>(byte1) << 8u) |
-			static_cast<unsigned int>(byte2)) ^ gamma;
+		unsigned int message_xored = message_to_int ^ gamma;
 
-		unsigned int encrypt = (message_xored << leftshift) | (message_xored & 0xFFFF) >> (16 - leftshift);
+		unsigned int encrypt = (message_xored << leftshift) | (message_xored & 0xFFFFFFFF) << (32 - leftshift);
 
 		//std::cout << message_xored << " xor \n" << encrypt << " enc \n";
 
-		char r1 = encrypt >> 8;
-		char r2 = encrypt;
-		std::cout << r1 << r2;
+		char r1 = encrypt << 24;
+		char r2 = encrypt << 16;
+		char r3 = encrypt << 8;
+		char r4 = encrypt;
+		std::cout << r1 << r2 << r3 << r4;
 		file.write(&r1, sizeof(r1));
 		file.write(&r2, sizeof(r2));
+		file.write(&r3, sizeof(r3));
+		file.write(&r4, sizeof(r4));
 	}
 	file.close();
 	std::cout << "\n";
